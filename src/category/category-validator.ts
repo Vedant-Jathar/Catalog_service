45d06fc1ead36aecbc24c02febf4c04a2d0cec17
Vedant_Jathar <jatharvedant16@gmail.com>
2025-07-07 23:08:41 +0530

    add create category validator

---
+++ src/category/category-validator.ts
@@ -0,0 +1,31 @@
+import { error } from "console"
+import { body } from "express-validator"
+import createHttpError from "http-errors"
+
+export const createCategoryValidator = [
+    body("name")
+        .exists()
+        .withMessage("Category Name is required")
+        .isString()
+        .withMessage("Category Name should be a required"),
+
+    body("priceConfiguration")
+        .exists()
+        .withMessage("Price configuration is required"),
+
+    body("priceConfiguration.*.priceType")
+        .exists()
+        .withMessage("Category Name is required")
+        .custom((value: "base" | "addistional") => {
+            const validTypes = ["base", "additional"]
+            if (!validTypes.includes(value)) {
+                throw createHttpError(400, `${value} is not allowed for the price type`)
+            }
+            return true
+        }),
+
+    body("attributes")
+        .exists()
+        .withMessage("Attributes are required"),
+
+]
