bdd6712f7acfa3e4c58ee1c48fbf4311b5ac6c0d
Vedant_Jathar <jatharvedant16@gmail.com>
2025-07-29 13:47:16 +0530

    eslint config change

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,6 @@ module.exports = {
         "@typescript-eslint/no-misused-promises": "off",
         "@typescript-eslint/ban-ts-comment": "off",
         "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-explicit-any": "off"
-    }
+        "@typescript-eslint/no-explicit-any": "off",
+    },
 };
